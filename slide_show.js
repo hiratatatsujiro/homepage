892213c6c002d775311d743dbda9eb0c1e14aff5
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2022-02-15 04:47:21 +0900

    sample画像の追加

--- slide_show.js
+++ slide_show.js
@@ -6,6 +6,12 @@ const app = new Vue({
     phone: "090-****-****",
     area: "images/access.png",
     wday: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
-    teacherImage: "images/pre_teacher.png"
+    teacherImage: "images/pre_teacher.png",
+    picture1: "images/sample1.png",
+    picture2: "images/sample2.png",
+    picture3: "images/sample3.png",
+    picture4: "images/sample4.png",
+    picture5: "images/sample5.png",
+    picture6: "images/sample6.png"
   }
 })
