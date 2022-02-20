7f2c60ec713a89ba0d67ab4e93626d8f1c542bcb
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2022-02-21 04:16:26 +0900

    微調整

--- slide_show.js
+++ slide_show.js
@@ -37,10 +37,8 @@ const app = new Vue({
   el: "#app",
   data: {
     name: "平田むつみ",
-    tel: "0942-77-0017",
-    phone: "090-****-****",
+    tel: "０９４２ー７７ー００１７",
     area: "images/access.png",
-    wday: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
     teacherImage: "images/pre_teacher.png",
     picture1: "images/sample1.png",
     picture2: "images/sample2.png",
